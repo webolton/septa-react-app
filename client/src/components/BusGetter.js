816f76a888b4f4061da9b208f8901d9a54aae25c
William Bolton <william.ellet@gmail.com>
2017-07-30 09:07:34 -0400

    Add form to bus getter

--- client/src/components/BusGetter.js
+++ client/src/components/BusGetter.js
@@ -3,17 +3,44 @@ import PropTypes from 'prop-types';
 
 class BusGetter extends Component {
 
+  static defaultProps = {
+    directions: [
+      "North",
+      "South",
+      "East",
+      "West"
+    ]
+  }
+
   render() {
+
+    let cardinalDirections = this.props.directions.map(direction => {
+      return <option key={direction} value={direction}>{direction}</option>
+    })
 
     return (
       <div>
         <h3>Find a bus!</h3>
+        <form>
+          <div>
+            <label>What route are you taking?</label>
+            <input type="text" ref="route" />
+          </div>
+          <div>
+            <label>What direction are you traveling?</label><br />
+            <select ref="direction">
+              {cardinalDirections}
+            </select>
+          </div>
+          <input type="submit" value="submit" />
+        </form>
       </div>
     );
   }
 }
 
 BusGetter.propTypes ={
+  directions: PropTypes.array,
 }
 
 export default BusGetter;
