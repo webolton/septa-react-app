f695f220da78791176b2734190b7042e1e00d23e
William Bolton <william.ellet@gmail.com>
2017-07-30 18:46:56 -0400

    Add Bootstrap classes to header

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import BusGetter from './components/BusGetter'
-import PhilaMap from './components/PhilaMap'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Septa, Where You At?</h2>
-        </div>
-        <BusGetter />
+        <nav className="App-header navbar navbar-fixed-top">
+          <div className="container">
+
+            <div className="navbar-header">
+              <div className="navbar-brand">
+                <h2><strong>SEPTA</strong>, Where You At?</h2>
+              </div>
+            </div>
+              <BusGetter />
+          </div>
+        </nav>
       </div>
     );
   }
