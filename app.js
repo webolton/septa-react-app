b96d3625f4e94046ddb40a7d47f8a442fa43428e
William Bolton <william.ellet@gmail.com>
2017-07-30 17:27:10 -0400

    Remove un-used files

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
 var septa_client = require('./routes/septa_client');
 
 var app = express();
@@ -22,7 +21,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
 app.use('/septa_client', septa_client);
 
 // catch 404 and forward to error handler
