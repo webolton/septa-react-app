8b7cfabfc26c87e1d0c34bc5fda3d145c82f13b7
William Bolton <william.ellet@gmail.com>
2017-07-30 13:38:42 -0400

    Styles for markers and marker text

---
+++ client/src/components/SeptaMarkerStyles.js
@@ -0,0 +1,15 @@
+const SeptaMarkerStyles = {
+  border: '1px solid #000',
+  width: 32,
+  borderRadius: 4,
+  backgroundColor: 'white',
+  backgroundImage: "url(septa.svg)",
+  backgroundSize: '100%',
+  backgroundRepeat: 'no-repeat',
+  textAlign: 'center',
+  color: '#3f51b5',
+  fontSize: 10,
+  fontWeight: 'bold',
+};
+
+export {SeptaMarkerStyles};
---
+++ client/src/components/SeptaMarkerTextStyles.js
@@ -0,0 +1,7 @@
+const SeptaMarkerTextStyles = {
+  paddingTop: 25,
+  textAlign: 'center',
+  lineHeight: '1'
+};
+
+export {SeptaMarkerTextStyles};
