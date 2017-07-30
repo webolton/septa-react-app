81d29130348853e60e2b60a35de72f73cd8c3b0a
William Bolton <william.ellet@gmail.com>
2017-07-30 09:16:45 -0400

    Get Septa data with septa_client

--- routes/septa_client.js
+++ routes/septa_client.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var requestify = require('requestify');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:route', function(req, res, next) {
+
+  var routeId = req.params["route"]
+  requestify.get(`http://www3.septa.org/beta/TransitView/${routeId}`).then(function(response) {
+    let septaResponse = response.getBody();
+    res.json(septaResponse["bus"]);
+  });
+
 });
 
 module.exports = router;
